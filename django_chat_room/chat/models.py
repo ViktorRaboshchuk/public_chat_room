18d3bc7cfc16c127adbc5c5d04cf5ebbee5c6955
Viktor <viktor.raboshchuk@gmail.com>
2021-03-30 20:46:20 +0300

    changing serializers and adding validators to models field

--- django_chat_room/chat/models.py
+++ django_chat_room/chat/models.py
@@ -1,13 +1,24 @@
+import re
+
 from django.contrib.auth.models import User
+from django.core.validators import EmailValidator
 from django.db import models
 
 # Create your models here.
+from rest_framework.exceptions import ValidationError
+
+
+def text_validator(text_area):
+    """Message validation (regex to check if message is not empty string, and length < 100)"""
+
+    if re.search(r"^\s*$", text_area) is not None:
+        raise ValidationError('String is empty')
 
 
 class Chat(models.Model):
     """Chat model"""
     username = models.CharField(max_length=150, default="")
-    text = models.TextField("Message", max_length=100)
-    email = models.EmailField("User email", blank=False, default="")
+    text = models.TextField("Message", max_length=100, validators=[text_validator])
+    email = models.EmailField("User email", blank=False, default="", validators=[EmailValidator])
     created_date = models.DateTimeField("Created date", auto_now_add=True)
     updated_date = models.DateTimeField("Updated date", auto_now_add=True)
--- django_chat_room/chat/serializers.py
+++ django_chat_room/chat/serializers.py
@@ -1,52 +1,25 @@
 """Serializers for converting querysets to JSON"""
-import re
 
 from rest_framework import serializers
-from chat.models import Chat
+from chat.models import Chat, text_validator
 
-from django.contrib.auth.models import User
-from django.core.validators import (validate_email,
-                                    EmailValidator)
-
-
-def text_validator(text_area):
-    """Message validation (regex to check if message is not empty string, and length < 100)"""
-
-    if re.search(r"^\s*$", text_area) is None:
-        raise serializers.ValidationError('String is empty')
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """User serializer"""
-
-    class Meta:
-        model = User
-        fields = ("id", "username")
+from django.core.validators import (validate_email)
 
 
 class ChatSerializer(serializers.ModelSerializer):
     """Chat serializer"""
 
-    # Email validation (regex to check if that is real email)
-    email = serializers.EmailField(validators=[validate_email])
-    text = serializers.CharField(validators=[text_validator])
-
     class Meta:
         model = Chat
-        fields = ("username", "text", "email", "created_date", "updated_date")
-        # extra_kwargs = {'email': {'validators': [EmailValidator, ]},}
+        fields = ("id", "username", "text", "email", "created_date")
 
 
 class ChatCreateSerializer(serializers.ModelSerializer):
     """Chat serializer"""
 
-    # Email validation (regex to check if that is real email)
-    email = serializers.EmailField(validators=[validate_email])
-    text = serializers.CharField(validators=[text_validator])
-
     class Meta:
         model = Chat
-        fields = ("username", "text", "email")
+        fields = '__all__'
 
 
 class ChatUpdateSerializer(serializers.ModelSerializer):
