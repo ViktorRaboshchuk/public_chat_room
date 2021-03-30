061bacf8c4d5a9dd982a07d773a4578289b67b87
Viktor <viktor.raboshchuk@gmail.com>
2021-03-30 21:22:10 +0300

    heroku changes

--- django_chat_room/django_chat_room/settings.py
+++ django_chat_room/django_chat_room/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -147,8 +148,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
